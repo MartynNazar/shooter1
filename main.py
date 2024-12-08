97de3d6c6e3269e6edfe5bef78d40e979e1ed188
MartynNazar <martynnazar11@gmail.com>
2024-12-08 14:42:20 +0200

    hkgy8v

---
+++ main.py
@@ -0,0 +1,104 @@
+import pygame
+
+class Rocket:
+    def __init__(self, speed, width, height, x, y, skin):
+        self.texture = pygame.image.load(skin)
+        self.texture = pygame.transform.scale(self.texture, [width,height])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+    def draw(self,window):
+        window.blit(self.texture,self.hitbox)
+
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.hitbox.x += self.speed
+        if keys[pygame.K_DOWN]:
+            self.hitbox.y += self.speed
+        if keys[pygame.K_LEFT]:
+            self.hitbox.x -= self.speed
+        if keys[pygame.K_UP]:
+            self.hitbox.y -= self.speed
+
+class UFO:
+    def __init__(self,width,height,x,y,skin):
+        self.texture = pygame.image.load(skin)
+        self.texture = pygame.transform.scale(self.texture, [width,height])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+
+
+
+    def draw(self,window):
+        window.blit(self.texture,self.hitbox)
+
+
+
+
+
+
+
+pygame.init()
+
+window = pygame.display.set_mode([700,500])
+fps = pygame.time.Clock()
+rocket = Rocket(1,50,50,100,250,"rocket.png")
+
+
+
+
+
+background = pygame.image.load("galaxy.jpg")
+background = pygame.transform.scale(background, [700,500])
+
+ufos = [
+    UFO(50,50,10,10,"ufo.png"),
+    UFO(50,50,65,10,"ufo.png"),
+    UFO(50,50,120,10,"ufo.png"),
+    UFO(50,50,175,10,"ufo.png"),
+    UFO(50,50,230,10,"ufo.png"),
+    UFO(50,50,285,10,"ufo.png"),
+    UFO(50, 50, 345, 10, "ufo.png"),
+    UFO(50, 50, 400, 10, "ufo.png"),
+    UFO(50, 50, 455, 10, "ufo.png"),
+    UFO(50, 50, 510, 10, "ufo.png"),
+    UFO(50, 50, 565, 10, "ufo.png"),
+    UFO(50, 50, 620, 10, "ufo.png"),
+
+
+
+]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x,y = pygame.mouse.get_pos()
+            print(x,y)
+    window.fill([123,123,123])
+    window.blit(background,[0,0])
+    rocket.draw(window)
+    rocket.move()
+
+    window.fill([123, 123, 123])
+    window.blit(background, [0, 0])
+    rocket.draw(window)
+
+
+
+
+
+
+    pygame.display.flip()
+
+
+    for ufo in ufos:
+        ufo.draw(window)
+    pygame.display.flip()
+
+
+    fps.tick()
